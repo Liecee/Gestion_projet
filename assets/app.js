9732b3689da1fc4e014e7183b431f4e9925af93d
Liecee <tiliouineliece@live.fr>
2024-01-17 16:53:06 +0100

    écriture style

--- assets/app.js
+++ assets/app.js
@@ -7,3 +7,10 @@
 
 // any CSS you import will output into a single css file (app.css in this case)
 import './styles/app.css';
+
+import $ from 'jquery';
+
+import '@fortawesome/fontawesome-free/js/fontawesome';
+import '@fortawesome/fontawesome-free/js/solid';
+import '@fortawesome/fontawesome-free/js/regular';
+import '@fortawesome/fontawesome-free/js/brands';
